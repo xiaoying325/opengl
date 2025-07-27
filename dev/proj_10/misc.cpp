79bd329f1c45209cce0eae15a240f3336814ee14
pengchao <pengchao@wepie.com>
2025-07-27 20:51:49 +0800

    总结课

---
+++ dev/proj_10/misc.cpp
@@ -0,0 +1,47 @@
+
+#include <stdio.h>
+#include "glew.h"
+
+
+
+
+GLuint CreateBufferObject(GLenum bufferType, GLsizeiptr size, GLenum usage, void* data /* = nullptr */)
+{
+	GLuint object;
+	glGenBuffers(1, &object);
+	glBindBuffer(bufferType, object);
+	glBufferData(bufferType, size, data, usage);
+	glBindBuffer(bufferType, 0);
+	return object;
+}
+
+
+char* LoadFileContent(const char* path)
+{
+	FILE* pFile = fopen(path, "rb");
+	if (pFile)
+	{
+		fseek(pFile, 0, SEEK_END);
+		int nLen = ftell(pFile);
+		char* buffer = nullptr;
+		if (nLen != 0)
+		{
+			buffer = new char[nLen + 1];
+			rewind(pFile);
+			fread(buffer, nLen, 1, pFile);
+			buffer[nLen] = '\0';
+		}
+		else
+		{
+			printf("load file fail %s content len is 0\n", path);
+		}
+		fclose(pFile);
+		return buffer;
+	}
+	else
+	{
+		printf("open file %s fail\n", path);
+	}
+	fclose(pFile);
+	return nullptr;
+}
