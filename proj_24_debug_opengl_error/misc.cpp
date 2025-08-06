2d8050e0ba2277c02b54996a8b4d2cc0d3596ba9
pengchao <pengchao@wepie.com>
2025-08-07 01:16:14 +0800

    misc源文件中写检查错误函数的定义void CheckGLError(const char* file, int line)

--- proj_24_debug_opengl_error/misc.cpp
+++ proj_24_debug_opengl_error/misc.cpp
@@ -149,7 +149,38 @@ GLuint CreateTextureFromFile(const char* imagePath) {
 
 
 }
+void CheckGLError(const char* file, int line)
+{
 
+	GLenum error = glGetError();
+	if (error != GL_NO_ERROR)
+	{
+		switch (error)
+		{
+		case  GL_INVALID_ENUM:
+			printf("GL Error GL_INVALID_ENUM %s : %d\n", file, line);
+			break;
+		case  GL_INVALID_VALUE:
+			printf("GL Error GL_INVALID_VALUE %s : %d\n", file, line);
+			break;
+		case  GL_INVALID_OPERATION:
+			printf("GL Error GL_INVALID_OPERATION %s : %d\n", file, line);
+			break;
+		case  GL_STACK_OVERFLOW:
+			printf("GL Error GL_STACK_OVERFLOW %s : %d\n", file, line);
+			break;
+		case  GL_STACK_UNDERFLOW:
+			printf("GL Error GL_STACK_UNDERFLOW %s : %d\n", file, line);
+			break;
+		case  GL_OUT_OF_MEMORY:
+			printf("GL Error GL_OUT_OF_MEMORY %s : %d\n", file, line);
+			break;
+		default:
+			printf("GL Error 0x%x %s : %d\n", error, file, line);
+			break;
+		}
+	}
+}
 
 //GLuint CreateTextureFromFile(const char* imagePath)
 //{
