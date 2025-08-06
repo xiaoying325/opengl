17efc1aaab372f58f7fbf62ee11370af2f1de60e
pengchao <pengchao@wepie.com>
2025-08-07 01:15:19 +0800

    misc中定义检查是否存在执行报错的函数和宏

--- proj_24_debug_opengl_error/misc.h
+++ proj_24_debug_opengl_error/misc.h
@@ -4,3 +4,6 @@
 GLuint CreateBufferObject(GLenum bufferType, GLsizeiptr size, GLenum usage, void* data = nullptr);
 char* LoadFileContent(const char* path);
 GLuint CreateTextureFromFile(const char* imagePath);
+
+void CheckGLError(const char* file, int line);
+#define GL_CALL(x) do{ x;CheckGLError(__FILE__,__LINE__);}while (0)
